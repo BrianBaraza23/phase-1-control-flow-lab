727b44c13cf2f5ffd2e235b42ace66d96cd4b36c
baraza7 <barazabrian87@gmail.com>
2023-06-13 12:47:43 +0300

    Completed assigments

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
-}
+  
+  if (ride <= 400) {
+    return 'This one is on me!';
+  }
+  else if (ride > 2000 && ride <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else { 
+      return 'No can do.';
+  }
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  function ternaryCheckCity(city){
+    let trueStatement=`Ok, sounds good.`
+    let falseStatement=`No go.`
+    return (city === 'NYC') ? trueStatement : falseStatement
+    
+  }
+  ternaryCheckCity(`NYC`)
+  ternaryCheckCity(`Pittsburgh`)
+
+  function switchOnCharmFromTip(tip){
+    // Write your code here!
+    
+    
+  
+    switch (tip){
+      case `generous`:
+        return `Thank you so much.`;
+        break;
+        case 'not as generous':
+          return `Thank you.`
+          break;
+          default:
+            return`Bye.`
+    }
+     
+    }
+  
+  switchOnCharmFromTip(`generous`)
+  switchOnCharmFromTip(`not as generous`)
+  switchOnCharmFromTip(`thanks for everything`)
